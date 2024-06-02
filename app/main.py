fdd4f775a143d499b2e8bd02d30d2ed47668ffd7
FieldMarshallObvious <noahdelangel@gmail.com>
2024-06-01 22:11:23 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
 import sys
 
 
-def main():
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+allowed_commands = []
 
-    # Wait for user input
-    input()
+def main():
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+
+        # Wait for user input
+        user_input = input()
+
+        if user_input not in allowed_commands:
+            sys.stdout.write(f"{user_input}: command not found\n")
+            syst.stdout.fluch()
 
 
 if __name__ == "__main__":
